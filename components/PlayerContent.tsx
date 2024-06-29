90bcccf0e711e777ada65cec84beb1d7add45298
Shivansh Gandotra <62881211+Shivansh3004@users.noreply.github.com>
2024-06-30 01:47:31 +0530

    fixed play icon

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -94,7 +94,7 @@ const PlayerContent:React.FC<PlayerContentProps> = ({song,songUrl}) => {
                 </div>
             </div>
             <div className="flex md:hidden col-auto w-full justify-end items-center">
-                <div onClick={handlePlay} className="h-40 w-10 flex items-center justify-center rounded-full bg-white p-1 cursor-pointer">
+                <div onClick={handlePlay} className="h-10 w-10 flex items-center justify-center rounded-full bg-white p-1 cursor-pointer">
                     <Icon size={30} className="text-black"/>
                 </div>
             </div>
